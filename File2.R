147a898ab78db9c9e9883ac15bbe5bb8d443dd26
AnuThomasJose <anuthms93@gmail.com>
2020-02-21 13:58:20 +0530

    2nd Day + GGPlot

--- File2.R
+++ File2.R
@@ -156,7 +156,117 @@ str(students)
 
 
 
+#day2:21Feb20----
+
+library(dplyr)
+table(mtcars$cyl)
+summary(mtcars$cyl)
+mtcars %>% group_by(cyl)  %>% tally()
+
+mtcars %>% group_by(cyl,gear) %>% tally()
+table(mtcars$cyl, mtcars$gear, dnn = c('cylinder','gear'))   #print as a matrix
+?mtcars
+
+xtabs( ~cyl+gear,data=mcars)
+ftable()
+
+
+df = mtcars
+head(df)  #sjws first 6 entries, tail gives last 6
+df$am = ifelse(df$am==0, 'Auto', 'Manual')   #replace 0,1 with auto/manual in the display
+df
+
+?mutate  #temporarily create a column by the given name and then operates on the dataframe
+mtcars %>% mutate(TxType = ifelse(am==0, 'Auto', 'Manual')) #doesnt disturb your original dataframe
+df = df%>%mutate(TxType = ifelse(am==0,'Auto','Manual'))#this will replace
+
+df = mtcars %>% mutate(mpg = mpg +10 )   #increase mpg by 10 without disturbing your oroginal values
+head(df) 
+
+
+df$mpg*1.1
+
+#add mpg + wt into new column MPGWT
+df$MPGWT = df$mpg+df$wt
+head(df)
+
+
+#Find out top two cars from each gear from the mileage point of view; use group_by & top_n
+
+df = mtcars
+df %>% group_by(gear)  %>% top_n(2,mpg)  #top_n, top_frac. if lowest 2 put -mpg
+
+#1. list out details of any 2 cars randomly 2. 25% cars
+
+sample_n(df,2,T)
+sample_frac(df,.25,T)
+
+#arrange the .25 students in ascending
+df %>% sample_frac(.25,T) %>% arrange(mpg)
+
+
+top_n(df,2,mpg)
+
+
+#ascending gear, descending mpg
+
+#mean hp, milage for each gear type
+df %>% group_by(gear) %>% summarise(avgMileage = mean(mpg))
+df %>% group_by(gear)%>% summarise(avgHp =mean(hp))
+
+df %>% group_by(gear)%>% summarise_at(c('mpg','wt','hp','disp'),c(mean))
+#find max and min of wt for each gear type
+df%>% select(mpg,wt,gear) %>% group_by(gear) %>% summarise_each(c(min,max)
+)
 
 
 
+
+#graphs----
+hist(df$mpg,col = 6:15)
+barplot(table(df$gear), col = 1:3)  
+
+pie(table(df$gear))
+plot(df$wt,df$mpg)
+
+library(ggplot2)
+library(reshape2)
+(rollno = paste('IIM',1:10,sep='-'))
+(name=paste('SName',1:10,sep=' '))
+(gender = sample(c('M','F'),size=10,replace=T))
+(program = sample(c('BBA','MBA'),size=10,replace=T))
+(marketing = trunc(rnorm(10,mean=60,sd=10)))
+(finance = trunc(rnorm(10,mean=55,sd=12)))
+students<-data.frame(rollno,name,gender,program,marketing,finance,stringsAsFactors = F)
+students
+head(students)
+
+
+(meltsum1 <- melt(students, id.vars=c('rollno','name','gender','program'),variable.name = 'subject',value.name = 'marks'))
+meltsum1
+head(students)
+students %>% group_by(gender) %>% summarise(COUNT = n())
+
+ggplot(students %>% group_by(gender) %>% summarise(COUNT = n()),aes(x=gender,y=COUNT,fill=program)) + geom_bar(stat='identity') + labs(title = 'Gender Wise Count')   #bar
+
+#stacked bar
+ggplot(students %>% group_by(gender,program) %>% summarise(COUNT = n()),aes(x=gender,y=COUNT,fill=program)) + geom_bar(stat='identity') + labs(title = 'Gender Wise Count')   #bar
+
+
+#side by side
+ggplot(students %>% group_by(program,gender) %>% summarise(COUNT = n()),aes(x=gender,y=COUNT,fill=program)) + geom_bar(stat='identity',position = position_dodge2(.7)) + labs(title = 'Gender Wise Count')  
+
+
+
+
+ggplot(students, aes(x=gender, y=..count..,fill=program)) + geom_bar(stat = 'count') + labs(title = 'Gender Wise - ProgramCount') 
+
+
+#to include marks also using the melted structure
+#subject - Program -Gender- Mean Marks
+names(meltsum1)
+head(meltsum1)
+ggplot(meltsum1 %>% group_by(program,gender,subject) %>% summarise(meanMarks = round(mean(marks))),aes(x=gender,y=meanMarks,fill=program)) + geom_bar(stat='identity',position = position_dodge2(.7)) + geom_text(aes(label = meanMarks), position = position_dodge2(.7)) + labs(title = 'Subject-Marks-Program-Gender Wise Count')+ facet_grid(~subject)
+
+ggplot(mtcars,aes(x=wt,y=mpg,size=hp,color = factor(gear),shape=factor(am)))+geom_point()
 
